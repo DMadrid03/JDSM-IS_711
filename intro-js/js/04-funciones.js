bb32170000755fcfe0da6c0ead8f332b09777bea
Daniel Madrid <danielmadrid@unah.hn>
2024-09-26 17:46:57 -0600

    clase 26-9 funciones flechas, introducción a parámetros en funciones

--- intro-js/js/04-funciones.js
+++ intro-js/js/04-funciones.js
@@ -12,7 +12,33 @@ const saludo2 = function(){
 //no se va a poder modificar el valor de la variable, esta variable tiene la referencia de la función
 
 
+//otra forma de definir las funciones: callback o función flecha
+const imprimir = (nombre = "")=>{
+    console.log('Hola desde una función flecha, ' + nombre)
+}
+
+const calcAreaCirc = (r)=>{
+
+    //validar valor de entrada
+        //?if(!r || !isNaN(r)) return 0
+        //o bien
+        if(!Number(r)) return 0 //*recomendada
+    const area = Math.PI * Math.pow(r,2)
+    return area
+}
+//misma función en una línea
+const calcAreaCirc2 = (r)=> Math.PI * Math.pow(r,2) ?? 0 //!Inculida la validación
+
+console.log(calcAreaCirc(2))
+console.log(calcAreaCirc("2"))
+console.log(calcAreaCirc("hola"))
+
 console.log(saludo()) //?muestra undefined por que la funcion no devuelve nada
 
 const miFuncion =saludo
 console.log(miFuncion())
+console.log(imprimir())
+
+
+console.log(eval('2+2'))
+
